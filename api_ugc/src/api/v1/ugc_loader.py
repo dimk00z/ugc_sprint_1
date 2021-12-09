467d22d2197264eac5dcb1c91ce7e2a08e45e861
Dimk00z <dimk00z@gmail.com>
2021-12-09 18:18:21 +0300

    Update endpoint

--- api_ugc/src/api/v1/ugc_loader.py
+++ api_ugc/src/api/v1/ugc_loader.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from http import HTTPStatus
+from uuid import UUID
+
+from fastapi import APIRouter, HTTPException
 from models.request import RequestForUGS
 
 from services.kafka_loader import kafka_loader
@@ -6,12 +9,31 @@ from services.kafka_loader import kafka_loader
 router = APIRouter()
 
 
-@router.post("/", summary="UGC Loader inner ednpoint")
+def create_answer(*, was_loaded: bool, movie_id: UUID, user_id: UUID) -> dict:
+    return {
+        "result": {
+            "was_loaded": was_loaded,
+            "movie_id": str(movie_id),
+            "user_id": str(user_id),
+        }
+    }
+
+
+@router.post("/load_inner", summary="UGC Loader inner ednpoint", status_code=201)
 def load_innerdata_to_kafka(request_for_ugs: RequestForUGS):
-    kafka_loader.load(request_for_ugs)
+    print(request_for_ugs)
+    was_loaded: bool = kafka_loader.load(request_for_ugs)
+    result = create_answer(
+        was_loaded=was_loaded,
+        movie_id=request_for_ugs.payload.movie_id,
+        user_id=request_for_ugs.payload.user_id,
+    )
+    if was_loaded:
+        return result
+    raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=result)
 
 
-@router.post("/load_outer", summary="UGC Loader outer ednpoint")
+@router.post("/load_outer", summary="UGC Loader outer ednpoint", status_code=201)
 def load_innerdata_to_kafka(request_for_ugs: RequestForUGS):
     # TODO parse jwt
     kafka_loader.load(request_for_ugs)
