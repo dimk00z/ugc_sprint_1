edee7e4b599c5ea0cbb603eb8ba7be6333f47025
Dimk00z <dimk00z@gmail.com>
2021-12-09 18:17:44 +0300

    Add backoff

---
+++ api_ugc/src/utils/back_off.py
@@ -0,0 +1,11 @@
+import logging
+
+import backoff
+
+
+def backoff_hdlr(details):
+    logging.error(
+        "Backing off {wait:0.1f} seconds after {tries} tries "
+        "calling function {target} with args {args} and kwargs "
+        "{kwargs}".format(**details)
+    )
