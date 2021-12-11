525ef70beab07eb7356dc8f72f7cb3eb563fff05
Dimk00z <dimk00z@gmail.com>
2021-12-11 18:27:26 +0300

    Add docstring

--- api_ugc/src/api/v1/ugc_loader.py
+++ api_ugc/src/api/v1/ugc_loader.py
@@ -3,8 +3,9 @@ from http import HTTPStatus
 from uuid import UUID
 
 from fastapi import APIRouter, HTTPException, Query
-from models.request import RequestForUGS, create_random_request
+from models.event import EventForUGS, create_random_event
 
+from core.settings import get_settings
 from services.ugc_kafka_producer import ugc_kafka_producer
 
 router = APIRouter()
@@ -22,15 +23,14 @@ def create_answer(*, was_loaded: bool, movie_id: UUID, user_id: UUID) -> dict:
 
 
 @router.post("/produce", summary="UGC produce endpoint", status_code=201)
-async def inner_produce(request_for_ugs: RequestForUGS):
-    logger.debug(request_for_ugs)
-    was_produced: bool = await ugc_kafka_producer.produce(
-        request_for_ugs=request_for_ugs
-    )
+async def inner_produce(event_for_ugs: EventForUGS):
+    """Эндпоит пишет сообщение об одном событии в Kafka"""
+    logger.debug(event_for_ugs)
+    was_produced: bool = await ugc_kafka_producer.produce(request_for_ugs=event_for_ugs)
     result = create_answer(
         was_loaded=was_produced,
-        movie_id=request_for_ugs.payload.movie_id,
-        user_id=request_for_ugs.payload.user_id,
+        movie_id=event_for_ugs.payload.movie_id,
+        user_id=event_for_ugs.payload.user_id,
     )
     if was_produced:
         return result
@@ -38,28 +38,31 @@ async def inner_produce(request_for_ugs: RequestForUGS):
 
 
 @router.post("/batch_produce", summary="UGC batch produce endpoint", status_code=201)
-async def batch_inner(requests_for_ugs: list[RequestForUGS]):
-    logger.debug(requests_for_ugs)
-    was_produced: bool = await ugc_kafka_producer.batch_produce(
-        requests=requests_for_ugs
+async def batch_inner(events_for_ugs: list[EventForUGS]):
+    """Эндпоит пишет список записей об событиях в Kafka"""
+    logger.debug(events_for_ugs)
+    was_produced: bool = await ugc_kafka_producer.batch_produce(requests=events_for_ugs)
+    result = {"batch_produced": was_produced}
+    if was_produced:
+        return result
+    raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=result)
+
+
+if get_settings().app.is_debug:
+    # WARNING only for debug
+    @router.post(
+        "/random_batch_produce",
+        summary="Debug UGC batch produce endpoint",
+        status_code=201,
     )
-    result = {"batch_produced": was_produced}
-    if was_produced:
-        return result
-    raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=result)
-
-
-# WARNING only for debug
-@router.post(
-    "/random_batch_produce", summary="UGC produce inner endpoint", status_code=201
-)
-async def random_batch_produce(
-    batch_count: int = Query(default=500, alias="batch_count")
-):
-    batch = [create_random_request() for _ in range(batch_count)]
-    print(len(batch))
-    was_produced: bool = await ugc_kafka_producer.batch_produce(requests=batch)
-    result = {"batch_produced": was_produced}
-    if was_produced:
-        return result
-    raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=result)
+    async def random_batch_produce(
+        batch_count: int = Query(default=500, alias="batch_count")
+    ):
+        """Эндпоит генерирует {batch_count} валидных сообщений для тестов"""
+        batch = [create_random_event() for _ in range(batch_count)]
+        print(len(batch))
+        was_produced: bool = await ugc_kafka_producer.batch_produce(requests=batch)
+        result = {"batch_produced": was_produced}
+        if was_produced:
+            return result
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=result)
