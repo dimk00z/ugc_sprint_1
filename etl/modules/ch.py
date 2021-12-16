4bb15e4c96943cc620ebe0ceaf5fc672e7373e91
weraleto <52561921+weraleto@users.noreply.github.com>
2021-12-16 21:06:16 +0300

    [#9] separate events by topics in ETL

--- etl/modules/ch.py
+++ etl/modules/ch.py
@@ -6,9 +6,10 @@ from clickhouse_driver.errors import Error
 
 
 class ETLClickhouseDriver:
-    def __init__(self, host: str, db: str):
+    def __init__(self, host: str, db: str, tables: list[str]):
         self.host = host
         self.db = db
+        self.tables = tables
         self.client = self.get_ch_client()
 
     @backoff.on_exception(backoff.expo, Error)
@@ -19,32 +20,35 @@ class ETLClickhouseDriver:
         self.client.execute(
             f"CREATE DATABASE IF NOT EXISTS {self.db} ON CLUSTER yandex_practicum"
         )
-        self.client.execute(
-            f"""
-            CREATE TABLE IF NOT EXISTS {self.db}.regular_table ON CLUSTER yandex_practicum
-                (
-                    language String,
-                    timezone String,
-                    ip String,
-                    version String,
-                    movie_id UUID,
-                    user_id UUID,
-                    event String,
-                    event_data String,
-                    event_timestamp String
-                )
-                Engine=MergeTree()
-            ORDER BY event_timestamp
-            """
-        )
+        for table in self.tables:
+            self.client.execute(
+                f"""
+                CREATE TABLE IF NOT EXISTS {self.db}.{table} ON CLUSTER yandex_practicum
+                    (
+                        language String,
+                        timezone String,
+                        ip String,
+                        version String,
+                        movie_id UUID,
+                        user_id UUID,
+                        event_data String,
+                        event_timestamp String
+                    )
+                    Engine=MergeTree()
+                ORDER BY event_timestamp
+                """
+            )
 
     def load(self, data: dict):
-        try:
-            self.client.execute(
-                f"INSERT INTO {self.db}.regular_table VALUES", data, types_check=True
-            )
-            return True
-        except KeyError as ch_err:
-            logging.error(
-                "Error while loading data into Clickhouse: {0}".format(ch_err)
-            )
+        for event_type, batch in data.items():
+            try:
+                self.client.execute(
+                    f"INSERT INTO {self.db}.{event_type} VALUES",
+                    batch,
+                    types_check=True,
+                )
+                return True
+            except KeyError as ch_err:
+                logging.error(
+                    "Error while loading data into Clickhouse: {0}".format(ch_err)
+                )
